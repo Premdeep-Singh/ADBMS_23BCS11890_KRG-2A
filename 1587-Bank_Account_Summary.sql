0e15dcdf93382585927b7169bd1f27baf298a82b
Premdeep_Singh <mcplayer0219@gmail.com>
2025-07-30 12:24:28 +0530

    1587-Bank_Account_Summary.sql

--- 1587-Bank_Account_Summary.sql
+++ 1587-Bank_Account_Summary.sql
@@ -2,7 +2,6 @@
 -- Write a solution to report the name and balance of users with a balance higher than 10000. 
 -- The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 
-# Write your MySQL query statement below
 SELECT NAME, SUM(amount) as BALANCE  FROM
 Users as U
     JOIN 
